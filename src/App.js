93af33dedd1a26164c0dc621d7b9f5ad262bdcf6
Marcellia <ankitaz.az@gmail.com>
2022-05-13 19:42:05 -0400

    Components and Routes created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { DarkTheme, LightTheme } from "./components/Themes";
+import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from "./globalStyles";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import {
+  Main,
+  AboutPage,
+  BlogPage,
+  MySkillsPage,
+  WorkPage,
+} from "./components";
 
 function App() {
   return (
     <div>
-      <GlobalStyle>Hello, World</GlobalStyle>
+      <GlobalStyle />
+
+      <ThemeProvider theme={LightTheme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/AboutPage" component={AboutPage} />
+          <Route exact path="/BlogPage" component={BlogPage} />
+          <Route exact path="/WorkPage" component={WorkPage} />
+          <Route exact path="/MySkillsPage" component={MySkillsPage} />
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Main } from "./Main";
+export { AboutPage } from "./AboutPage";
+export { BlogPage } from "./BlogPage";
+export { MySkillsPage } from "./MySkillsPage";
+export { WorkPage } from "./WorkPage";
