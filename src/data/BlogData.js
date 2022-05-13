a91785479d5ce712a4156b2d58eb8505dc8fd7cc
Marcellia <ankitaz.az@gmail.com>
2022-05-13 16:17:18 -0400

    React app initialized

---
+++ src/data/BlogData.js
@@ -0,0 +1,36 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"Web Accessibility — Make your web application more accessible",
+        tags:["Accessibility","HTML","CSS"],
+        date:"Jan, 2022",
+        imgSrc:"https://giphy.com/embed/HlcFs8tJ1Dfu8" ,
+        link:"https://medium.com/geekculture/web-accessibility-make-your-web-application-more-accessible-af24b4292db0"
+    },
+    {
+        id:2,
+        name:"NodeJS, Express, Axios and the basic set-up",
+        tags:["NodeJS","ExpressJS","Axios"],
+        date:"Dec, 2021",
+        imgSrc:"https://miro.medium.com/max/700/1*C_QkULkvThWbHYBGXFpFiQ.png",
+        link:"https://medium.com/geekculture/nodejs-express-axios-and-the-basic-set-up-fe88a0b75dca"
+    },
+    {
+        id:3,
+        name:"Counter App with ReactJS",
+        tags:["ReactJS","HTML","JavaScript"],
+        date:"Nov, 2021",
+        imgSrc:"https://miro.medium.com/max/700/1*e-S_ABkPeXliI0VlubJ1Bw.png",
+        link:"https://medium.com/geekculture/counter-app-with-reactjs-d7835059edcd"
+    },
+    {
+        id:4,
+        name:"Create your very first React App",
+        tags:["ReactJS","HTML","JavaScript"],
+        date:"Nov, 2021",
+        imgSrc:"https://miro.medium.com/max/614/1*XCZD7qOyuDyCQhKNXN1WnA.png",
+        link:"https://medium.com/geekculture/create-your-very-first-react-app-e50759faf50e"
+    },
+   
+  
+] 
